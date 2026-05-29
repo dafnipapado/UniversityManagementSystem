d35c26102f6228946abd2df0309f334f6df2f58f
DaphnePap <daphnepapadop@gmail.com>
2026-05-29 14:31:08 +0300

    feat: add security configuration with custom handlers and userDetailsService

---
+++ src/main/java/com/myapp/university/authentication/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.myapp.university.authentication;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableMethodSecurity
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final AuthenticationSuccessHandler authenticationSuccessHandler;
+    private final AuthenticationFailureHandler authenticationFailureHandler;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+            .authorizeHttpRequests((request) -> request
+                    .requestMatchers("/index").permitAll()
+                    .requestMatchers("/login").permitAll()
+                    .requestMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
+                    .requestMatchers("/users/**").hasAuthority("ROLE_ADMIN")
+                    .anyRequest().authenticated()
+            )
+            .formLogin((form) -> form
+                    .loginPage("/login")
+                    .successHandler(authenticationSuccessHandler)
+                    .failureHandler(authenticationFailureHandler)
+            )
+            .logout((logout) -> logout
+                    .logoutSuccessUrl("/login?logout")
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID")
+
+            );
+
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+}
