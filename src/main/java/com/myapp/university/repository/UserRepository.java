e3f394fa948cc0c2ad81a63fd700e38044e3e14d
DaphnePap <daphnepapadop@gmail.com>
2026-06-01 18:56:23 +0300

    feat: add EntityGraph for roles and matching capabilities in findByUsername query

--- src/main/java/com/myapp/university/repository/UserRepository.java
+++ src/main/java/com/myapp/university/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.myapp.university.repository;
 
 import com.myapp.university.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @EntityGraph(attributePaths = {"role", "role.capabilities"})
     Optional<User> findByUsername(String username);
 }
