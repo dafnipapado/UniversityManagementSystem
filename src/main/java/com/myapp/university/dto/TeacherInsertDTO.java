b53f088847607f51728c41bfb0fc7fdf635302b0
DaphnePap <daphnepapadop@gmail.com>
2026-06-01 19:04:22 +0300

    fix: remove password and roleId from TeacherInsertDTO's parameters

--- src/main/java/com/myapp/university/dto/TeacherInsertDTO.java
+++ src/main/java/com/myapp/university/dto/TeacherInsertDTO.java
@@ -2,9 +2,9 @@ package com.myapp.university.dto;
 
 import java.util.UUID;
 
-public record TeacherInsertDTO (String firstname, String lastname, String teacherAM, String afm, String email, String telephone, String zipCode, Long regionId, String username, String password, Long roleId){
+public record TeacherInsertDTO (String firstname, String lastname, String teacherAM, String afm, String email, String telephone, String zipCode, Long regionId, String username){
 
     public static TeacherInsertDTO empty() {
-        return new TeacherInsertDTO("", "", "", "", "", "","", 0L, "", "", 0L);
+        return new TeacherInsertDTO("", "", "", "", "", "","", 0L, "");
     }
 }
