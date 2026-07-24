40295f04037421a5eab2a9adb21002b22d18dbd4
DaphnePap <daphnepapadop@gmail.com>
2026-07-24 23:45:06 +0300

    fix: add auditing annotations for enrollment_date

--- src/main/java/io/github/dafnipapado/university/model/Enrollment.java
+++ src/main/java/io/github/dafnipapado/university/model/Enrollment.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 
@@ -13,6 +15,7 @@ import java.time.Instant;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "enrollments")
 public class Enrollment {
 
@@ -20,6 +23,7 @@ public class Enrollment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @CreatedDate
     @Column(columnDefinition = "DATETIME", nullable = false)
     private Instant enrollment_date;
 
